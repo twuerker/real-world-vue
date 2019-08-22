460354630933963631ee7b01cdaa5c8bec890e4c
Taylor Wuerker <taylor_wuerker@comcast.com>
2019-08-22 17:47:04 -0400

    Editor Lesson

---
+++ src/components/EventCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data() {
+    return {
+      quote: 'I want ice cream'
+    }
+  }
+}
+</script>
+
+<style></style>
